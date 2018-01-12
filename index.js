d9e3b6fd2baa3fd98aad99af8f5f3f9021b7e8aa
Wesley Kay <wesley@WK.local>
2018-01-12 11:46:57 -0700

    copy repo to overlay.github.io, init

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function dropDownFunc() {
+    document.getElementById("menuContent").classList.toggle("show");
+
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.droptab')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
---
+++ roadmap.js
@@ -0,0 +1,3 @@
+function showRoadmapInfo(){
+	document.getElementsById('circle-color-1').classList.toggle("show");
+}
